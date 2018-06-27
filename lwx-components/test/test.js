656e0b00c6d36d21b3e32fdeef4cf4202bec0bc4
Live-forever <18056613516@163.com>
2018-06-27 13:44:16 +0800

    随便写...

--- lwx-components/test/test.js
+++ lwx-components/test/test.js
@@ -1,1 +1,18 @@
 // test gitstatus
+sdfkj nskdnf sdn fkdsn kfnsd knsdklf nsldk flkadfmlk fkl ddasl fl l lal klk fksdfjdfhakjhf
+sdkjfnskjdfnk 
+fsdkfjjnsdfjkn sdkjfnskjdfnk
+
+sdfnsdnffs
+
+sdfmsd,fm 
+
+sdfms, dmlfsdm
+sdfmlsdfm
+sdf sdnf WebGLShadersdf n,sdnf sdffnsdf
+sdfsdfnasd fsdkfjjnsdfjknfsdf sdfsdfnasdfsdf
+sfsdfsndfldsml fsdklfjsdfnsd,f fhuiwehmndmfnsad 
+
+sdfnsdfnsdnf.defaultProps = {
+
+};
